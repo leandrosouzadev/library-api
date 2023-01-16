c42cc6094ce873fe2c78ef9b647c9ea84e82aa1b
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-01-16 17:01:48 -0300

    54 Teste de integração do repository para os metodos de save e delete

--- src/test/java/br/com/elegacy/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/br/com/elegacy/libraryapi/model/repository/BookRepositoryTest.java
@@ -19,54 +19,83 @@ class BookRepositoryTest {
 
 	@Autowired
 	private TestEntityManager testEntityManager;
-	
+
 	@Autowired
 	private BookRepository bookRepository;
-	
+
 	@Test
 	@DisplayName("Should return true when there is a book with informed isbn")
 	void shouldReturnTrueWhenIsbnExists() {
-		// Arrange		
+		// Arrange
 		String isbn = "123";
-		
+
 		Book book = createNewBook(isbn);
-		
+
 		testEntityManager.persist(book);
-		
+
 		// Act
 		boolean exists = bookRepository.existsByIsbn(isbn);
-		
+
 		// Assert
 		assertThat(exists).isTrue();
 	}
-	
+
 	@Test
 	@DisplayName("It should return false when there is no book with informed isbn")
 	void shouldReturnFalseWhenIsbnDoesntExists() {
-		// Arrange		
+		// Arrange
 		String isbn = "123";
-		
+
 		// Act
 		boolean exists = bookRepository.existsByIsbn(isbn);
-		
+
 		// Assert
 		assertThat(exists).isFalse();
 	}
-	
+
 	@Test
 	@DisplayName("Should get a book by id.")
 	void shouldGetBookById() {
 		// Arrange
 		Book book = createNewBook("123");
 		testEntityManager.persist(book);
-		
+
 		// Act
 		Optional<Book> foundBook = bookRepository.findById(book.getId());
-		
+
 		// Assert
-		assertThat(foundBook).isPresent();		
+		assertThat(foundBook).isPresent();
 	}
-	
+
+	@Test
+	@DisplayName("Should save a book.")
+	void shouldSaveBook() {
+		// Arrange
+		Book book = createNewBook("123");
+
+		// Act
+		Book savedBook = bookRepository.save(book);
+
+		// Assert
+		assertThat(savedBook.getId()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("Should delete a book.")
+	void shouldDeleteBook() {
+		// Arrange
+		Book book = createNewBook("123");
+		testEntityManager.persist(book);
+		Book foundBook = testEntityManager.find(Book.class, book.getId());
+
+		// Act
+		bookRepository.delete(foundBook);
+		Book deletedBook = testEntityManager.find(Book.class, foundBook.getId());
+
+		// Assert
+		assertThat(deletedBook).isNull();
+	}
+
 	private Book createNewBook(String isbn) {
 		Book book = Book
 				.builder()
