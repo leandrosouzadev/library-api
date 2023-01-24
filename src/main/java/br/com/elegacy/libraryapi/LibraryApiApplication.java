0378032618362d7570709e28ac5577c4e117fb92
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-01-24 17:04:32 -0300

    97 Publicando modificacoes no github

--- src/main/java/br/com/elegacy/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/elegacy/libraryapi/LibraryApiApplication.java
@@ -1,37 +1,18 @@
 package br.com.elegacy.libraryapi;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import br.com.elegacy.libraryapi.service.EmailService;
-
 @SpringBootApplication
 @EnableScheduling
-public class LibraryApiApplication {
-	
-	@Autowired
-	private EmailService emailService;
+public class LibraryApiApplication {	
 
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-	
-    @Bean
-    CommandLineRunner runner() {
-    	return args -> {
-    		List<String> emails = Arrays.asList("f1a1981a79-782ecd@inbox.mailtrap.io");
-    		emailService.sendMails("Testando o serviço de emails.", emails);
-    		//System.err.println("Emails enviados");
-    	};
     }
 
 	public static void main(String[] args) {
