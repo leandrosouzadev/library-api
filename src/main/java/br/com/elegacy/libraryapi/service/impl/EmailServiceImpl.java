5b80f651816520e95b7dd50e6e56825808dea8c0
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-01-24 16:10:53 -0300

    85 Implementando o serviço de email

--- src/main/java/br/com/elegacy/libraryapi/service/impl/EmailServiceImpl.java
+++ src/main/java/br/com/elegacy/libraryapi/service/impl/EmailServiceImpl.java
@@ -2,13 +2,34 @@ package br.com.elegacy.libraryapi.service.impl;
 
 import java.util.List;
 
-import br.com.elegacy.libraryapi.service.EmailService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+import br.com.elegacy.libraryapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+	private final JavaMailSender javaMailSender;
+
+	@Value("${application.mail.default-remetent}")
+	private String remetent;
 
 	@Override
 	public void sendMails(String message, List<String> mailsList) {
+		String[] mails = mailsList.toArray(new String[mailsList.size()]);
 
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+		simpleMailMessage.setFrom(remetent);
+		simpleMailMessage.setSubject("Book with overdue loan.");
+		simpleMailMessage.setText(message);
+		simpleMailMessage.setTo(mails);
+
+		javaMailSender.send(simpleMailMessage);
 	}
 
 }
