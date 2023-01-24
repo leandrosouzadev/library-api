92d0d58a36ddf069494902b606032b80525182b0
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-01-24 17:03:05 -0300

    86 Testando o envio de emails

--- src/main/java/br/com/elegacy/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/elegacy/libraryapi/LibraryApiApplication.java
@@ -1,20 +1,38 @@
 package br.com.elegacy.libraryapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import br.com.elegacy.libraryapi.service.EmailService;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
+	
+	@Autowired
+	private EmailService emailService;
 
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
     }
 	
+    @Bean
+    CommandLineRunner runner() {
+    	return args -> {
+    		List<String> emails = Arrays.asList("f1a1981a79-782ecd@inbox.mailtrap.io");
+    		emailService.sendMails("Testando o serviço de emails.", emails);
+    		//System.err.println("Emails enviados");
+    	};
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
--- src/main/java/br/com/elegacy/libraryapi/service/ScheduleService.java
+++ src/main/java/br/com/elegacy/libraryapi/service/ScheduleService.java
@@ -7,20 +7,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import br.com.elegacy.libraryapi.model.entity.Loan;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ScheduleService {
 
 	private static final String CRON_LATE_LOANS = "0 0 0 1/1 * ?";
 
+	@NonNull
 	private final LoanService loanService;
+	
+	@NonNull
 	private final EmailService emailService;
-
+	
 	@Value("${application.mail.lateloans.message}")
 	private String message;
-
+	
 	@Scheduled(cron = CRON_LATE_LOANS)
 	public void sendMailToLateLoans() {
 		List<Loan> allLateLoans = loanService.getAllLateLoans();
